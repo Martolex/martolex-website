5fa974cc7e7a8dd3fcb247b563bfe33322399b52
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-08-19 20:45:21 +0530

    footer of  component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.scss";
 import Header from "./components/header/Header";
 import { ViewportProvider } from "./components/utils/viewPortHandler";
 import ProductListing from "./components/productListing/productListing";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <ViewportProvider>
       <div className="App">
         <Header />
         <ProductListing />
+        <Footer />
       </div>
     </ViewportProvider>
   );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,96 @@
+import React from "react";
+import "./footer.scss";
+import { Container, Col, Row, Form, Button } from "react-bootstrap";
+
+const Footer = (props) => {
+  return (
+    <Container fluid className="bg-dark p-5">
+      <Row>
+        <Col md={4} className="pr-5">
+          <Row>
+            <h3 className=" text-light">About Martolex</h3>
+          </Row>
+          <Row>
+            <p>
+              Martolex is an online book rental marketplace where you can buy
+              books and return them after use to save up to 70% of your book
+              expenditures in your college or school. Helping students save
+              time, save money and get smarter.
+            </p>
+          </Row>
+        </Col>
+        <Col md={3}>
+          <Row>
+            <h3 className=" text-light">Information</h3>
+          </Row>
+          <Row>
+            <a className="footer-link">Terms and conditions</a>
+          </Row>
+          <Row>
+            <a className="footer-link">Privacy Policy</a>
+          </Row>
+          <Row>
+            <a className="footer-link">Returns Policy</a>
+          </Row>
+          <Row>
+            <a className="footer-link">Damage Policy</a>
+          </Row>
+
+          <Row></Row>
+        </Col>
+        <Col md={2}>
+          <Row>
+            <h3 className=" text-light">Useful Links</h3>
+          </Row>
+          <Row>
+            <a className="footer-link">Media</a>
+          </Row>
+          <Row>
+            <a className="footer-link">FAQ</a>
+          </Row>
+          <Row>
+            <a className="footer-link">Rental Policy</a>
+          </Row>
+        </Col>
+        <Col md={3} className="">
+          <Row>
+            <h3 className=" text-light">Hear from Us</h3>
+          </Row>
+          <Row>
+            <p>
+              Be the first to know about the launch of books of other
+              educational fields and upcoming offers to save your money on them!
+              Rent smart and save smart!
+            </p>
+          </Row>
+
+          <Form>
+            <Row className="my-2">
+              <Form.Control
+                // className="ml-2"
+                type="text"
+                // size="lg"
+                placeholder="your Name"
+              />
+            </Row>
+            <Row>
+              <Form.Control
+                // className="ml-2"
+                type="text"
+                // size="lg"
+                placeholder="your email"
+              />
+            </Row>
+            <Row className="my-2">
+              <Button variant="primary" block>
+                SUBSCRIBE
+              </Button>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Footer;
