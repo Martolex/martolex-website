51ed670948f724dd88a159ce0e0e12003c8c1af6
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-08-19 14:19:04 +0530

    product list

---
+++ src/components/productListItem/productListLitem.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactStars from "react-stars";
+import "./productListItem.scss";
+
+const Product = (props) => {
+  return (
+    <div className="product">
+      <div className="product-img">
+        <img src="/book1.png"></img>
+      </div>
+      <div className="product-info">
+        <a className="prod-name">
+          A Textbook Of Data Communication And Networks
+        </a>
+        <p className="prod-author">Author: Dr. Sanjay Sharma</p>
+        <p className="prod-publisher">Publisher: SK Kataria and sons</p>
+        <ReactStars
+          count={5}
+          half={false}
+          onChange={(rating) => console.log(rating)}
+          size={30}
+          color2={"#ffd700"}
+        />
+        <div className="prices">
+          <span className="price">&#8377;299/-</span>
+          <span className="actual-price">&#8377;299/-</span>
+          <span className="discount">save 63%</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
---
+++ src/components/productListing/productListing.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./productListing.scss";
+import Product from "../productListItem/productListLitem";
+
+const ProductListing = (props) => {
+  return (
+    <div className="productListing">
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+};
+
+export default ProductListing;
