f6f420d20a362edc4e53b1365fe338eab73f2833
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-09-24 13:18:42 +0530

    modify backend URL

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export const backendApi =
   process.env.REACT_BACKEND_URL ||
-  "https://martolexbackend-env.eba-8mtjabh4.ap-south-1.elasticbeanstalk.com/";
+  (process.env.NODE_ENV == "development"
+    ? "http:localhost:3001"
+    : "http://martolexbackend-env.eba-8mtjabh4.ap-south-1.elasticbeanstalk.com/");
