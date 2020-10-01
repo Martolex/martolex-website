b774b37c7458c124992ac4c3175b2494d79c8131
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-10-01 11:58:12 +0530

    http to https backend

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ export const backendApi =
   process.env.REACT_BACKEND_URL ||
   (process.env.NODE_ENV == "development"
     ? "http://localhost:3000/"
-    : "http://martolexbackend-env.eba-8mtjabh4.ap-south-1.elasticbeanstalk.com/");
+    : "https://backend.martolex.com/");
