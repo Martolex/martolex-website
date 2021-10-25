45cc6eb43b045f79816eb6f6b3117a786e8515d3
Deepanshu Vangani <vanganideepanshu@gmail.com>
2021-10-25 10:11:36 +0530

    update backend

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export const backendApi =
   process.env.REACT_BACKEND_URL ||
   (process.env.NODE_ENV == "development"
     ? "http://localhost:3000/"
-    : "https://backend.martolex.com/");
+    : "http://martolexbackend-env.eba-8mtjabh4.ap-south-1.elasticbeanstalk.com/");
 
 export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || null;
